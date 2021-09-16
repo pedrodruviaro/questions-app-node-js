d803788d0f1b5a376d9f261a2b11f7ade219c649
Pedro Henrique <82953655+pedrodruviaro@users.noreply.github.com>
2021-09-15 21:37:52 -0300

    definindo estilos padrao

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const route = express.Router()
 
-route.get('/', (req, res) => res.send("Hello"))
+route.get('/', (req, res) => res.render("index"))
 
 
 
--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const route = require('./routes')
+const path = require('path')
 
 const server = express()
 const PORT = process.env.PORT || 3000
 
+server.set('view engine', 'ejs')
+server.set('views', path.join(__dirname, 'views'))
+
+server.use(express.static('public'))
+
 server.use(express.urlencoded({extended: true}))
 server.use(express.json())
 server.use(route)
+
 
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
